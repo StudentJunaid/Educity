afd772f8e0bf057ca4de044669b65ed30f93a8d4
Junaid Noor <junaidkhan97991@gmail.com>
2024-03-20 05:44:59 -0700

    adding smooth scrool

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll'
 
 function Navbar() {
   const [stikey, setStikey] = useState(false)
@@ -13,14 +14,14 @@ function Navbar() {
   
   return (
     <nav className={`container ${stikey ? 'dark-theme': ""}`}>
-       <img src={logo} alt="logo" className='logo' />
+       <img src={logo} alt="logo" className='logo' /> 
        <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonial</li>
-        <li><button className='btn'>Contect us</button></li>
+        <li><Link to='hero' smooth={true} offset={0}  duration={500} >Home</Link></li>
+        <li><Link to='program' smooth={true} offset={-260}  duration={500} >Program</Link></li>
+        <li><Link to='about' smooth={true} offset={-150}  duration={500} >About Us</Link></li>
+        <li><Link to='Campus' smooth={true} offset={-260}  duration={500} >Campus</Link></li>
+        <li><Link to='testimonial' smooth={true} offset={-260}  duration={500} >Testimonial</Link></li>
+        <li><Link className='btn' to='Contact' smooth={true} offset={-260}  duration={500} >Contect us</Link></li>
        </ul>
     </nav>
   )
